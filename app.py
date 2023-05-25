fd203363ea9e5a233c09f1f4ae5fb4059ae216ee
Leandro Deferrari <leandro_deferrari@hotmail.com>
2023-05-25 19:27:42 -0300

    Modificando ruta del home

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/index", methods=["GET"])
+@app.route("/", methods=["GET"])
 def home():
     return render_template("index.html")
 
